a20a2dfd8f0a6b7d7d8070d9a5b87b03c75debb1
David Skaalid Amundsen <david.s.amundsen@nasa.gov>
2017-04-18 15:17:07 -0400

    Fixed calculation of number of monochromatic calculations with the random overlap method in calc_num_k_terms.py.

--- calc_num_k_terms.py
+++ calc_num_k_terms.py
@@ -21,7 +21,7 @@ while True:
 fin.close()
 
 # Create array for number of k-terms
-n_k_terms = zeros([n_band, n_gas])
+n_k_terms = zeros([n_band, n_gas], dtype='int')
 
 # Find Block 5
 fin = open(spec_file, 'r')
@@ -70,7 +70,7 @@ print(n_k_terms)
 # overlap without resorting and rebinning
 n_mono_ro = 0
 for i_band in arange(n_band):
-  n_mono_ro = n_mono_ro + prod(nonzero(n_k_terms[i_band,:]))
+  n_mono_ro = n_mono_ro + prod(n_k_terms[i_band, n_k_terms[i_band,:] > 0])
 
 print('Number of monchromatic calculations with RO', n_mono_ro)
 
