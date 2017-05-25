d2ef0879b495029d39a802308865e210a4b3c22c
David Skaalid Amundsen <david.s.amundsen@nasa.gov>
2017-05-25 16:55:51 -0400

    Added support for generalised continua in calc_num_k_terms.py.

--- calc_num_k_terms.py
+++ calc_num_k_terms.py
@@ -18,10 +18,25 @@ while True:
   if line[:36] == 'Total number of gaseous absorbers = ':
     n_gas = int(line[36:])
     break
+  elif line == '*END':
+    n_gas = 0
+    break
+fin.close()
+
+# Get number of continua
+fin = open(spec_file, 'r')
+while True:
+  line = fin.readline()
+  if line[:39] == 'Total number of generalised continua = ':
+    n_cont = int(line[39:])
+    break
+  elif line == '*END':
+    n_cont = 0
+    break
 fin.close()
 
 # Create array for number of k-terms
-n_k_terms = zeros([n_band, n_gas], dtype='int')
+n_k_terms = zeros([n_band, n_gas+n_cont], dtype='int')
 
 # Find Block 5
 fin = open(spec_file, 'r')
@@ -59,9 +74,48 @@ while True:
   for i in arange(lines_to_skip):
     fin.readline()
 
+# Find Block 19
+fin = open(spec_file, 'r')
+while True:
+  line = fin.readline()
+  if line[:19] == '*BLOCK: TYPE =   19':
+    break
+
+# Skip Block 19 header
+for i in arange(4):
+  fin.readline()
+
+# Read data for all bands and all continua
+while True:
+  line = fin.readline()
+  
+  # Check if end of Block 19 has been reached
+  if line[:4] == '*END':
+    break
+  try:
+    i_band = int(line[:5])
+  except:
+    continue
+  i_cont = int(line[6:17])
+  i_overlap = int(line[36:])
+  
+  # Do not include k-terms for perfectly correlated continua
+  if i_overlap > 0:
+    lines_to_skip = int(line[18:29])
+  else:
+    n_k_terms[i_band-1, n_gas+i_cont-1] = int(line[18:29])
+    lines_to_skip = n_k_terms[i_band-1, n_gas+i_cont-1]
+
+  # Skip k-terms
+  for i in arange(lines_to_skip):
+    fin.readline()
+
 # Print results
 for i_gas in arange(n_gas):
   print('Gas ', i_gas + 1, ' k-terms:', int(sum(n_k_terms[:, i_gas])))
+for i_cont in arange(n_cont):
+  print('Continuum ', i_cont + 1, ' k-terms:', 
+      int(sum(n_k_terms[:,n_gas+i_cont])))
 print('Total number of k-terms: ', int(sum(sum(n_k_terms))))
 
 print(n_k_terms)
